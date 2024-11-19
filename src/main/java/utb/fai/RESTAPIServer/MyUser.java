d67685b380344b5f73208f7ac52d25113e708ae5
Michal <michalsichm@gmail.com>
2024-11-19 19:37:23 +0100

    controller is con(trolling)

--- src/main/java/utb/fai/RESTAPIServer/MyUser.java
+++ src/main/java/utb/fai/RESTAPIServer/MyUser.java
@@ -1,14 +1,23 @@
 package utb.fai.RESTAPIServer;
 
+import java.util.regex.Pattern;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table
 public class MyUser {
 
+    @Id
     private Long id;
     private String name;
     private String email;
     private String phoneNumber;
 
-    public MyUser() {}
+    public MyUser() {
+    }
 
     public MyUser(String name, String email, String phoneNumber) {
         this.name = name;
@@ -16,12 +25,53 @@ public class MyUser {
         this.phoneNumber = phoneNumber;
     }
 
-    public boolean isUserDataValid() {
-        // Add your validation logic here (e.g., email and phone number format
-        // validation)
-        return true;
+    public static boolean isUserDataValid(String email, String phoneNumber, String name) {
+        if (phoneNumber == null || phoneNumber.isEmpty()) {
+            return false;
+        }
+        String phoneRegex = "^\\+?\\d+$";
+        boolean phoneNumberIsValid = Pattern.matches(phoneRegex, phoneNumber);
+
+        if (email == null || email.isEmpty()) {
+            return false;
+        }
+
+        String emailRegex = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$";
+        boolean emailIsValid = Pattern.matches(emailRegex, email);
+        return emailIsValid && phoneNumberIsValid && name != null && !name.isEmpty();
     }
 
-    // TODO: Getters and setters
-    
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String newPhoneNumber) {
+        this.phoneNumber = newPhoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String newEmail) {
+        this.email = newEmail;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String newName) {
+        this.name = newName;
+    }
+
 }
--- src/main/java/utb/fai/RESTAPIServer/UserController.java
+++ src/main/java/utb/fai/RESTAPIServer/UserController.java
@@ -1,11 +1,15 @@
 package utb.fai.RESTAPIServer;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,24 +17,84 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
 
+    @Autowired
+    private final UserRepository userRepository;
+    
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @GetMapping("/users")
     public List<MyUser> getAllUsers() {
-        // TODO: implement logic of /users endpoint
-        return null;
+        return userRepository.findAll();
     }
 
     @GetMapping("/getUser")
     public ResponseEntity<MyUser> getUserById(@RequestParam Long id) {
-        // TODO: implement logic of /getUser endpoint
-        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        // return userRepository.findById(id)
+        //     .map(u -> new ResponseEntity<>(u, HttpStatus.OK))
+        //     .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+
+        try {
+            Optional<MyUser> user = this.userRepository.findById(id);
+            if (user.isPresent()) {
+                return new ResponseEntity<>(user.get(), HttpStatus.OK);
+            }
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            // TODO: handle exception
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        // return new ResponseEntity<>()
+
     }
 
     @PostMapping("/createUser")
     public ResponseEntity<MyUser> createUser(@RequestBody MyUser newUser) {
+        if (!MyUser.isUserDataValid(newUser.getEmail(), newUser.getPhoneNumber(), newUser.getName())) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
         // TODO: implement logic of /createUser endpoint
-        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        userRepository.save(newUser);
+
+        return new ResponseEntity<>(newUser, HttpStatus.CREATED);
     }
 
-    // TODO: Define remaining endpoints in the same way. For id parameter use annotation @RequestParam with name "id" and for MyUser structure use @RequestBody.
+    @PutMapping("/editUser")
+    public ResponseEntity<MyUser> editUser(@RequestParam Long id, @RequestBody MyUser editUser) {
+        if (!MyUser.isUserDataValid(editUser.getEmail(), editUser.getPhoneNumber(), editUser.getName())) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        Optional<MyUser> myUser = userRepository.findById(id);
+        if (myUser.isPresent()) {
+            MyUser user = myUser.get();
+            user.setEmail(editUser.getEmail());
+            user.setPhoneNumber(editUser.getPhoneNumber());
+            user.setName(editUser.getName());
+            userRepository.save(user);
+            return new ResponseEntity<>(user, HttpStatus.OK);
+        }
 
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @DeleteMapping("/deleteUser")
+    public ResponseEntity<MyUser> deleteUser(@RequestParam Long id) {
+        try {
+            userRepository.deleteById(id);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @DeleteMapping("/deleteAll")
+    public ResponseEntity<MyUser> deleteAllUsers() {
+        try {
+            userRepository.deleteAll();
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
 }
